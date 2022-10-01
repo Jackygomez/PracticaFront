9ab9dc8d8d14c1d042fa02f59440fdc658243b44
Jackeline <jackegomez.191102@gmail.com>
2022-10-01 16:07:08 -0500

    Trayendo las estaciones y estilizando las networks

--- src/componentes/networks.js
+++ src/componentes/networks.js
@@ -1,8 +1,10 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
+import { Card, Button } from 'react-bootstrap'
 
-function Networks(){
+function Networks() {
 
     const [networks, setNetworks] = useState([])
+    const [estaciones, setEstaciones] = useState([])
 
     useEffect(() => {
         getData()
@@ -17,8 +19,38 @@ function Networks(){
         console.log(networks)
     }
 
-    return(
-        <></>
+    const getEstaciones = async (id) => {
+        let url = `http://api.citybik.es/v2/networks/${id}`
+        const response = await fetch(url, { method: 'GET' })
+            .then(response => response.json())
+            .catch(error => console.log(error))
+        setEstaciones(response.network)
+        console.log(estaciones)
+    }
+
+    return (
+        <div className='cards__container'>
+            {
+                networks.map((network) => {
+                    return (
+                        <div key={network.id}>
+                            <center>
+                                <Card className='Card'>
+                                    <Card.Title>{network.name}</Card.Title>
+                                    <Card.Body>
+                                        <h5>PAIS: {network.location.country}</h5>
+                                        <h5>CIUDAD: {network.location.city}</h5>
+                                        <h5>RED: {network.name}</h5>
+                                        <h5>COMPAÑIA: {network.company}</h5>
+                                    </Card.Body>
+                                    <Button onClick={() => getEstaciones(network.id)}>Estaciones</Button>
+                                </Card>
+                            </center>
+                        </div>
+                    )
+                })
+            }
+        </div>
     )
 }
 export default Networks
